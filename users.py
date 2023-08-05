d85f8e70945cd78a1ee4425303f7634b9524add3
Tomas Vazquez <vazquezt2018@gmail.com>
2023-08-05 13:06:26 -0300

    get user by id added

--- users.py
+++ users.py
@@ -5,15 +5,25 @@ app = FastAPI()
 
 
 class User(BaseModel):
+    id: int
     name: str
     age: int
     isDeveloper: bool
 
 
-users = [User(name="Tomas", age=21, isDeveloper=True),
-         User(name="Manuel", age=23, isDeveloper=False)]
+users = [User(id=1, name="Tomas", age=21, isDeveloper=True),
+         User(id=2, name="Manuel", age=23, isDeveloper=False)]
 
 
 @app.get('/')
 async def get_users():
     return {"users": users}
+
+
+@app.get('/user/{id}')
+async def get_user_by_id(id: int):
+    usuarios = filter(lambda user: user.id == id, users)
+    try:
+        return list(usuarios)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
