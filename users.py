a4c941a0e8e1b9e20cc3d6f9a4b78aea1c567a3a
Tomas Vazquez <vazquezt2018@gmail.com>
2023-08-05 13:28:34 -0300

    post method

--- users.py
+++ users.py
@@ -30,6 +30,15 @@ async def get_user_by_query(id: int):
     return search_user(id)
 
 
+@app.post("/user/")
+async def add_user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users.append(user)
+        return {"Usuario agregado": user}
+
+
 def search_user(id: int):
     usuarios = filter(lambda user: user.id == id, users)
     try:
