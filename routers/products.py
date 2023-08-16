5974dc7467db73a2911e684142b0a6f756dcfe2a
Tomas Vazquez <vazquezt2018@gmail.com>
2023-08-16 15:41:11 -0300

    add/ raise exception

--- routers/products.py
+++ routers/products.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-router = APIRouter()
+router = APIRouter(prefix="/products")
 
 
-@router.get("/products")
+products_list = ["Producto1", "Producto2", "Producto3"]
+
+
+@router.get("/")
 async def products():
-    return ["Producto1", "Producto2", "Producto3",]
+    return products_list
+
+
+@router.get("/{id}")
+async def products(id: int):
+    if id > len(products_list):
+        raise HTTPException(404, detail="No se encuentra el producto.")
+
+    return products_list[id]
